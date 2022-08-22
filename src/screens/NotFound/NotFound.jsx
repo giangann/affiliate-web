3e7833c956fb4189ef4e3ec32d8f1235599f5860
baopham <pqbao27@gmail.com>
2022-08-23 00:41:09 +0700

    🚀 refactor code and create router

---
+++ src/screens/NotFound/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Stack, Typography } from '@mui/material'
+
+export const NotFound = () => {
+  return (
+    <Stack>
+      <Typography>Page Not Found</Typography>
+    </Stack>
+  )
+}
