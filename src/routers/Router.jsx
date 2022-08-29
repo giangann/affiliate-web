9ebce05c84da4b543bb7071f3cbb9d3ea0fb5aa9
baopham <pqbao27@gmail.com>
2022-08-30 03:08:49 +0700

    add data api reviews

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -10,7 +10,7 @@ export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/websites/:id" element={<Detail />} />
+      <Route path="/websites/show/:id" element={<Detail />} />
       <Route path="/:slug" element={<Detail />} />
       <Route path="/resources" element={<Resource />} />
       <Route path="*" element={<NotFound />} />
