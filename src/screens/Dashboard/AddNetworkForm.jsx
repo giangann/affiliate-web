f2dd772076737150b17bd6c81603a1432e623bde
giangann <70595717+giangann@users.noreply.github.com>
2022-08-31 00:14:01 +0700

    [Add feature] - [Admin: add new network]

--- src/screens/Dashboard/AddNetworkForm.jsx
+++ src/screens/Dashboard/AddNetworkForm.jsx
@@ -1,15 +1,19 @@
-import { Button, Grid, Input, Stack, styled, Typography } from '@mui/material'
-import React from 'react'
+import { Button, Grid, Input, MenuItem, Select, Stack, styled, Typography } from '@mui/material'
+import React, { useEffect } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import BoxWithHeader from '~/components/Box/BoxWithHeader'
 import { ResponsiveContainer } from '~/components/Layouts'
 import { baseColor } from '~/styles'
+import { makeStyles } from '@material-ui/core/styles'
+import { getApiResource, getGoogleLoginUrl, login, loginWithGG } from '~/apis'
 
 function AddNetworkForm() {
-  const grid = { xs: 12, md: 6 }
-  const gridFull = { xs: 12, md: 12 }
+  const [categories, setCategories] = React.useState([])
+  const [trackingSoftwares, setTrackingSoftware] = React.useState([])
+  const [paymentMethods, setPaymentMethod] = React.useState([])
+  const [paymentFrequencies, setPaymentFrequencies] = React.useState([])
 
-  const { handleSubmit, control } = useForm({
+  const { handleSubmit, control, watch, setValue } = useForm({
     email: 'onSubmit',
     reValidateMode: 'onChange',
     defaultValues: {}
@@ -18,6 +22,33 @@ function AddNetworkForm() {
   const onSubmit = (data) => {
     console.log(data)
   }
+
+  useEffect(() => {
+    const getCategories = async () => {
+      const listOfCategory = await getApiResource('categories')
+      setCategories(listOfCategory)
+    }
+    const getTrackingSoftwares = async () => {
+      const listOfTrackingSoftware = await getApiResource('categories')
+      setTrackingSoftware(listOfTrackingSoftware)
+    }
+    const getPaymentMethods = async () => {
+      const listOfPaymentMethod = await getApiResource('categories')
+      setPaymentMethod(listOfPaymentMethod)
+    }
+    const getPaymentFrequencies = async () => {
+      const listOfPaymentFrequencies = await getApiResource('categories')
+      setPaymentFrequencies(listOfPaymentFrequencies)
+    }
+
+    getCategories()
+    getTrackingSoftwares()
+    getPaymentMethods()
+    getPaymentFrequencies()
+  }, [])
+
+  const grid = { xs: 12, md: 6 }
+  const gridFull = { xs: 12, md: 12 }
   return (
     <BoxWithHeader
       mainColor={baseColor.blue}
@@ -31,7 +62,7 @@ function AddNetworkForm() {
         </Grid>
       )}
       footer={() => (
-        <Grid container component="form" spacing={2}>
+        <Grid container component="form" spacing={2} onSubmit={handleSubmit(onSubmit)}>
           <Grid item {...grid}>
             <Controller
               control={control}
@@ -108,6 +139,7 @@ function AddNetworkForm() {
               name="referral_commissione"
               render={({ field: { onChange, value } }) => (
                 <CustomInput
+                  type="number"
                   disableUnderline={true}
                   placeholder="Referral commissione"
                   onChange={onChange}
@@ -122,6 +154,7 @@ function AddNetworkForm() {
               name="minimum_payment"
               render={({ field: { onChange, value } }) => (
                 <CustomInput
+                  type="number"
                   disableUnderline={true}
                   placeholder="Minimum payment"
                   onChange={onChange}
@@ -135,12 +168,22 @@ function AddNetworkForm() {
               control={control}
               name="payment_method_id"
               render={({ field: { onChange, value } }) => (
-                <CustomInput
-                  disableUnderline={true}
-                  placeholder="Payment Method"
+                <CustomSelect
+                  displayEmpty
+                  renderValue={
+                    watch('payment_method_id')
+                      ? undefined
+                      : () => <Placeholder>Payment method</Placeholder>
+                  }
                   onChange={onChange}
                   value={value}
-                />
+                >
+                  {paymentMethods.map((item) => (
+                    <MenuItem key={item.id} value={item.id}>
+                      {item.name}
+                    </MenuItem>
+                  ))}
+                </CustomSelect>
               )}
             />
           </Grid>
@@ -149,12 +192,22 @@ function AddNetworkForm() {
               control={control}
               name="payment_frequency_id"
               render={({ field: { onChange, value } }) => (
-                <CustomInput
-                  disableUnderline={true}
-                  placeholder="Minimum payment"
+                <CustomSelect
+                  displayEmpty
+                  renderValue={
+                    watch('payment_frequency_id')
+                      ? undefined
+                      : () => <Placeholder>Payment frequency</Placeholder>
+                  }
                   onChange={onChange}
                   value={value}
-                />
+                >
+                  {paymentFrequencies.map((item) => (
+                    <MenuItem key={item.id} value={item.id}>
+                      {item.name}
+                    </MenuItem>
+                  ))}
+                </CustomSelect>
               )}
             />
           </Grid>
@@ -163,30 +216,48 @@ function AddNetworkForm() {
               control={control}
               name="tracking_software_id"
               render={({ field: { onChange, value } }) => (
-                <CustomInput
-                  disableUnderline={true}
-                  placeholder="Tracking Software"
+                <CustomSelect
+                  displayEmpty
+                  renderValue={
+                    watch('tracking_software_id')
+                      ? undefined
+                      : () => <Placeholder>Tracking software</Placeholder>
+                  }
                   onChange={onChange}
                   value={value}
-                />
-              )}
-            />
-          </Grid>
-          <Grid item {...gridFull}>
-            <Controller
-              control={control}
-              name="category_id"
-              render={({ field: { onChange, value } }) => (
-                <CustomInput
-                  disableUnderline={true}
-                  placeholder="Category"
-                  onChange={onChange}
-                  value={value}
-                />
+                >
+                  {trackingSoftwares.map((item) => (
+                    <MenuItem key={item.id} value={item.id}>
+                      {item.name}
+                    </MenuItem>
+                  ))}
+                </CustomSelect>
               )}
             />
           </Grid>
           <Grid item {...grid}>
+            <Controller
+              control={control}
+              name="category_id"
+              render={({ field: { onChange, value } }) => (
+                <CustomSelect
+                  displayEmpty
+                  renderValue={
+                    watch('category_id') ? undefined : () => <Placeholder>Category</Placeholder>
+                  }
+                  onChange={onChange}
+                  value={value}
+                >
+                  {categories.map((item) => (
+                    <MenuItem key={item.id} value={item.id}>
+                      {item.name}
+                    </MenuItem>
+                  ))}
+                </CustomSelect>
+              )}
+            />
+          </Grid>
+          <Grid item {...gridFull}>
             <Button type="submit" variant="contained">
               Add
             </Button>
@@ -196,11 +267,29 @@ function AddNetworkForm() {
     />
   )
 }
+const Placeholder = ({ children }) => {
+  const classes = usePlaceholderStyles()
+  return <div className={classes.placeholder}>{children}</div>
+}
+const usePlaceholderStyles = makeStyles((theme) => ({
+  placeholder: {
+    color: '#aaa'
+  }
+}))
 
 const CustomInput = styled(Input)({
   '& .css-1x51dt5-MuiInputBase-input-MuiInput-input': {
     padding: '16px 8px'
   },
+  fontWeight: 600,
+  fontSize: '0.825rem',
+  width: '100%',
+  backgroundColor: '#f8fafc',
+  border: '1px solid #dae1e7',
+  borderRadius: '0.25rem'
+})
+
+const CustomSelect = styled(Select)({
   fontWeight: 600,
   fontSize: '0.825rem',
   width: '100%',
